eb34156e906e1250c1f804febbb1ce2450cbba01
Albab Saifi <saifialbab@gmail.com>
2024-06-26 14:22:56 -0400

    Change folder structure

--- merchant-center-custom-application/src/constants.js
+++ merchant-center-custom-application/src/constants.js
@@ -1,10 +1,6 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
-// export const entryPointUriPath = 'starter-a05e1b';
-export const entryPointUriPath =
-  typeof window === 'undefined'
-    ? process.env.ENTRY_POINT_URI_PATH
-    : window.app.entryPointUriPath;
+export const entryPointUriPath = 'starter-a05e1b';
 
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
