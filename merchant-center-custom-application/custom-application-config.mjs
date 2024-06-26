a84eef73dcc160aa8f091743f52700a0383b8743
Albab Saifi <saifialbab@gmail.com>
2024-06-26 16:53:18 -0400

    url update

--- merchant-center-custom-application/custom-application-config.mjs
+++ merchant-center-custom-application/custom-application-config.mjs
@@ -13,7 +13,7 @@ const config = {
     },
     production: {
       applicationId: 'clxvafwyd001m907kugt0x3n7',
-      url: '${env:CUSTOM_APPLICATION_ID}',
+      url: '${env:APPLICATION_URL}',
     },
   },
   oAuthScopes: {
