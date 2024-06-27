859e10abe3381d79530f5249723ead30d3835a10
Albab Saifi <saifialbab@gmail.com>
2024-06-27 15:18:11 -0400

    Update entry point

--- merchant-center-custom-application/src/constants.js
+++ merchant-center-custom-application/src/constants.js
@@ -1,6 +1,6 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
-export const entryPointUriPath = 'starter-a05e1b';
+export const entryPointUriPath = 'det';
 
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
